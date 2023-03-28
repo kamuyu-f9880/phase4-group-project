65f6195680db444114654f2b9fe58828ba608700
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-03-28 14:37:58 +0300

    Reeltime its Up

--- reeltime-api/app/controllers/reviews_controller.rb
+++ reeltime-api/app/controllers/reviews_controller.rb
@@ -1,46 +1,58 @@
 class ReviewsController < ApplicationController
     before_action :set_movie
-    before_action :set_review, only: [:edit, :update, :destroy]
+    before_action :set_review, only: [:show, :update, :destroy]
   
+    # GET /movies/:movie_id/reviews
+    def index
+      reviews = movie.reviews
+      render json: reviews
+    end
+  
+    # GET /movies/:movie_id/reviews/:id
+    def show
+      render json: review
+    end
+  
+    # POST /movies/:movie_id/reviews
     def create
       review = movie.reviews.new(review_params)
-      review.user = current_user
   
       if review.save
-        redirect_to movie, notice: 'Review was successfully created.'
+        render json: review, status: :created, location: movie_review_url(@movie, @review)
       else
-        render 'movies/show'
+        render json: review.errors, status: :unprocessable_entity
       end
     end
   
-    def edit
-    end
-  
+    # PATCH/PUT /movies/:movie_id/reviews/:id
     def update
       if review.update(review_params)
-        redirect_to movie, notice: 'Review was successfully updated.'
+        render json: review
       else
-        render 'movies/show'
+        render json: review.errors, status: :unprocessable_entity
       end
     end
   
+    # DELETE /movies/:movie_id/reviews/:id
     def destroy
       review.destroy
-      redirect_to movie, notice: 'Review was successfully destroyed.'
+      head :no_content
     end
   
     private
-      def set_movie
-        movie = Movie.find(params[:movie_id])
-        render json: movie
-      end
   
-      def set_review
-        review = current_user.reviews.find(params[:id])
-        render json: review
-      end
+    def set_movie
+      movie = Movie.find(params[:movie_id])
+      render json: movie, status: :ok
+    end
   
-      def review_params
-        params.require(:review).permit(:title, :body, :rating)
-      end
+    def set_review
+      review = movie.reviews.find(params[:id])
+      render json: review
+    end
+  
+    def review_params
+      params.require(:review).permit(:title, :body, :rating)
+    end
 end
+  
