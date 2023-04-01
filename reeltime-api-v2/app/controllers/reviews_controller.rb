669ca71ea695d4c8241358088ff78f79cf48ea55
fred <daktarispacetime@gmail.com>
2023-04-01 15:54:25 +0300

    prep for routing

--- reeltime-api-v2/app/controllers/reviews_controller.rb
+++ reeltime-api-v2/app/controllers/reviews_controller.rb
@@ -1,13 +1,17 @@
 class ReviewsController < ApplicationController
  # GET /movies/:movie_id/reviews
  def index
-    reviews = movie.reviews
+    reviews = Movie.reviews
     render json: reviews
   end
 
   # GET /movies/:movie_id/reviews/:id
   def show
-    render json: review
+    render json: {
+      user_id: reviews.user_id,
+      name: reviews.name,
+      comments: reviews.comments
+    }
   end
 
   # POST /movies/:movie_id/reviews
@@ -25,3 +29,4 @@ class ReviewsController < ApplicationController
       params.require(:review).permit(:user_id, :movie_id, :comments, :rating)
     end
   end
+end
--- reeltime-api-v2/config/routes.rb
+++ reeltime-api-v2/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
+  get 'movies', to: 'movies#index'
+  get 'actors', to: 'actors#index'
+  get 'users', to: 'users#index'
+  get 'reviews', to: 'reviews#index'
+
+  
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
