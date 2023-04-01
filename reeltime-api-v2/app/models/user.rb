093b13cf28f9905aece92bb343b8f219544a897e
fred <daktarispacetime@gmail.com>
2023-04-01 14:42:36 +0300

    added validation to user model

--- reeltime-api-v2/app/models/user.rb
+++ reeltime-api-v2/app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
 
     has_many :reviews
     has_many :movies, through: :reviews
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
 end
