276e821e3e43392ef410dd608f991f7b4e8a28b1
fred <daktarispacetime@gmail.com>
2023-04-02 10:07:58 +0300

    added password_digest to user table and removed password

--- reeltime-api-v2/db/schema.rb
+++ reeltime-api-v2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_01_074706) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_02_070705) do
   create_table "actors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -42,9 +42,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_01_074706) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
