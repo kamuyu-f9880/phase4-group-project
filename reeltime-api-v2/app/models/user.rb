49c7ace82762953da00fa9cd578733a35995eb3c
fred <daktarispacetime@gmail.com>
2023-04-02 11:04:18 +0300

    added confirmation mailer route

--- reeltime-api-v2/app/models/user.rb
+++ reeltime-api-v2/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    CONFIRMATION_TOKEN_EXPIRATION = 20.minutes
+
+    has_secure_password
+
     before_save :downcase_email
 
     has_many :movies
@@ -8,7 +12,27 @@ class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
-    validates :password, presence: true
+   
+
+    def confirm!
+        update_columns(confirmed_at: Time.current)
+    end
+
+    def confirmed?
+        confirmed_at.present?
+    end
+
+    def generate_confirm_token
+        signed_id expires_in: CONFIRMATION_TOKEN_EXPIRATION, purpose: :confirm_email
+    end
+  
+    def unconfirmed?
+      !confirmed?
+    end 
+    
+    
+    private
+
 
     def downcase_email
         self.email = email.downcase
--- reeltime-api-v2/config/routes.rb
+++ reeltime-api-v2/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # confirmation mailer routes
+
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
 
   get 'movies', to: 'movies#index'
   get 'actors', to: 'actors#index'
