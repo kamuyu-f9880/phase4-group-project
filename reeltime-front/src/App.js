168a2568027b667c572a0d4b98390494d61aabc4
feisalbireh <feisal.bireh@student.moringaschool.com>
2023-04-02 15:47:54 +0300

    Updating crud functionalities

--- reeltime-front/src/App.js
+++ reeltime-front/src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Home from './components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import EmpListing from './EmpListing';
+import EmpCreate from './EmpCreate';
+import EmpDetail from './EmpDetail';
+import EmpEdit from './EmpEdit';
+import Login from "./Login";
+import Signup from "./Signup";
+// import Home from "./Home";
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <h1>Reel Time</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />}/>
-          <Route exact path="/login" element={<Login />}/>
-          <Route path="/home" element={<Home />}/>
+          <Route path="/" element={<EmpListing />}></Route>
+          <Route path="/employee/create" element={<EmpCreate />}></Route>
+
+          <Route path="/employee/detail/:empid" element={<EmpDetail />}></Route>
+          <Route path="/employee/edit/:empid" element={<EmpEdit />}></Route>
+          <Route path="/signup" element={<Signup />} />
+          <Route exact path="/login" element={<Login />} />
+          {/* <Route path="/home" element={<Home />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
   );
-}
 
+}
 
 export default App;
---
+++ reeltime-front/src/EmpDetail.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import "./EmpDetails.css";
+
+const EmpDetail = () => {
+  const { empid } = useParams();
+
+  const [empdata, empdatachange] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:8000/employee" + empid)
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        empdatachange(resp);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-8">
+          <div className="card shadow-sm">
+            <div className="card-body">
+              {empdata && (
+                <div>
+                  <h2 className="movie-title">{empdata.name}</h2>
+                  <div className="movie-details">
+                    <p className="movie-genre">{empdata.genre}</p>
+                    <p className="movie-release-date">{empdata.releasedate}</p>
+                  </div>
+                  <Link className="btn btn-danger back-btn" to="/">
+                    Back to Listing
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmpDetail;
