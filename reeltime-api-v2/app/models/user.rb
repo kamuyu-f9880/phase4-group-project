5dfa727db93bfc4e3d5d9d25522ff42e3b667e32
fred <daktarispacetime@gmail.com>
2023-04-02 10:28:19 +0300

    added email validation stuff

--- reeltime-api-v2/app/models/user.rb
+++ reeltime-api-v2/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save :downcase_email
 
     has_many :movies
 
@@ -6,6 +7,10 @@ class User < ApplicationRecord
     has_many :movies, through: :reviews
 
     validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
     validates :password, presence: true
+
+    def downcase_email
+        self.email = email.downcase
+    end
 end
